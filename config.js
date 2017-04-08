8189de6095a1d133937ac111cbaeacf2e59a83bd
shahnirav <shah.nirav@me.com>
2017-04-09 00:16:23 +0100

    M

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = [];
+
+config['dev'] = {
+    mongoUrl: 'mongodb://localhost/drops'
+
+};
+
+config['production'] = {
+    mongoUrl: 'mongodb://localhost/drops'
+};
+
+module.exports = config;
